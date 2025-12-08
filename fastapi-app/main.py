2ca54435bf3c5f2e2726ca44014dccb97fc307d5
Dmitrii <wonderrfau1t@gmail.com>
2025-12-08 20:09:42 +0700

    Switched to HTTPS in CORS

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 app.middleware("http")(auth_middleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
